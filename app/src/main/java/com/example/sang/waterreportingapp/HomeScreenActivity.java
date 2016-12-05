4961ef0fb3a561536238b01e53b8776a9a51788b
mwang370 <mwang370@gatech.edu>
2016-12-04 19:27:08 -0500

    finished welcome and login screen

--- app/src/main/java/com/example/sang/waterreportingapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/sang/waterreportingapp/HomeScreenActivity.java
@@ -1,7 +1,10 @@
 package com.example.sang.waterreportingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class HomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        final Button logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent cancelIntent = new Intent(HomeScreenActivity.this, MainScreenActivity.class);
+
+                HomeScreenActivity.this.startActivity(cancelIntent);
+            }
+        });
     }
 }
