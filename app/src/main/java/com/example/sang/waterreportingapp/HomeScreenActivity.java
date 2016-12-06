a3ec73f207c9c3f70bac2ceadc755d5e56fbc43e
mwang370 <mwang370@gatech.edu>
2016-12-05 21:45:05 -0500

    commit

--- app/src/main/java/com/example/sang/waterreportingapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/sang/waterreportingapp/HomeScreenActivity.java
@@ -5,8 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+    private static DatabaseReference db = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,47 @@ public class HomeScreenActivity extends AppCompatActivity {
                 HomeScreenActivity.this.startActivity(updateProfileIntent);
             }
         });
+
+        final Button mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewMapIntent = new Intent(HomeScreenActivity.this, MapsActivity.class);
+                HomeScreenActivity.this.startActivity(viewMapIntent);
+            }
+        });
+
+        final Button qualityReportButton = (Button) findViewById(R.id.qualityReportButton);
+        qualityReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // if this user is a manager
+                DatabaseReference ref = db.child("users").child(getIntent().getExtras().getString("username")).child("userType");
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        if (snapshot.getValue().toString().equals("Manager")) {
+                            Intent viewQualityReportIntent = new Intent(HomeScreenActivity.this, QualityReportActivity.class);
+                            HomeScreenActivity.this.startActivity(viewQualityReportIntent);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError firebaseError) {
+                    }
+                });
+            }
+        });
+
+        final Button allReportsButton = (Button) findViewById(R.id.allReportsButton);
+        allReportsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewAllReportsIntent = new Intent(HomeScreenActivity.this, AllReportsActivity.class);
+                HomeScreenActivity.this.startActivity(viewAllReportsIntent);
+            }
+        });
+
 
         final Button logoutButton = (Button) findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener(){
---
+++ app/src/main/java/com/example/sang/waterreportingapp/QualityReportActivity.java
@@ -0,0 +1,13 @@
+package com.example.sang.waterreportingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QualityReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quality_report);
+    }
+}
